87b1baa92d0cd944cadd43892ec11642d7e6125b
Banah Abdeljaber <babdeljaber14@gmail.com>
2024-01-12 16:37:37 -0700

    finished one project

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function alertButton() {
-    alert("helloWorld")
-}
+edUcate.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup) {
+        myPopup.classList.remove("show");
+    }
+});
