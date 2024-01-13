c555c921cb383c4dbfc7bd5ff68ba8f875e7643e
Banah Abdeljaber <babdeljaber14@gmail.com>
2024-01-12 20:38:33 -0700

    I finishedgit add .

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// edUcate
 edUcate.addEventListener("click", function () {
     myPopup.classList.add("show");
 });
@@ -7,5 +8,31 @@ closePopup.addEventListener("click", function () {
 window.addEventListener("click", function (event) {
     if (event.target == myPopup) {
         myPopup.classList.remove("show");
+    }
+});
+
+// SE lab
+SE.addEventListener("click", function () {
+    myPopup2.classList.add("show");
+});
+closePopup2.addEventListener("click", function () {
+    myPopup2.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup2) {
+        myPopup2.classList.remove("show");
+    }
+});
+
+// MM lab
+MM.addEventListener("click", function () {
+    myPopup3.classList.add("show");
+});
+closePopup3.addEventListener("click", function () {
+    myPopup3.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup3) {
+        myPopup3.classList.remove("show");
     }
 });
